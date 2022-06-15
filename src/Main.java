33aaf3213f868da877b6f486a75fcf8441456c3d
ibmsc <Ibm.sc1409@gmail.com>
2022-06-15 21:48:58 +0300

    2 коммит

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Задача № 1!");
 
+        int[] weigth = new int[3];
+
+        double[] weight = {1.57, 7.654, 9.986};
+
+        short[] weight2 = new short[5];
+
+        System.out.println("Задача № 2!");
     }
 }
