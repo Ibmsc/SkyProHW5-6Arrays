7ac4e87b8c0006ef70c33190706d3a5035cea1fd
ibmsc <Ibm.sc1409@gmail.com>
2022-06-21 10:45:23 +0300

    4 коммит

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         System.out.println("Задача № 1!");
 
         int[] arrays = new int[3];
+        int arrSize = arrays.length;
         arrays[0] = 1;
         arrays[1] = 2;
         arrays[2] = 3;
@@ -10,19 +11,27 @@ public class Main {
         double[] weightsD = {1.57, 7.654, 9.986};
 
         short[] weightsS = new short[] {4,5,6,7,8};
-
+        int arrSizeS = arrays.length;
         System.out.println("Задача № 2!");
 
         for (int i = 0; i < 1; i++) {
-            System.out.print(" " + arrays[0] + ", " + arrays[1] + ", " + arrays[2] + " " );
+            System.out.println(" " + arrays[0] + ", " + arrays[1] + ", " + arrays[2] + " " );
 
         for (double d = 0; d < 1; d++) {
-            System.out.print(" " + weightsD[0] + ", " + weightsD[1] + ", " + weightsD[2] + " ");
+            System.out.println(" " + weightsD[0] + ", " + weightsD[1] + ", " + weightsD[2] + " ");
 
             for (short s = 0; s < 1; s++) {
-                System.out.print(" " + weightsS[0] + ", " + " " + weightsS[1] + ", " + " " + weightsS[2] + ", " + " "
+                System.out.println(" " + weightsS[0] + ", " + " " + weightsS[1] + ", " + " " + weightsS[2] + ", " + " "
                         + weightsS[3] + ", " + " " + weightsS[4] + " ");
-            }
-        }  }
-    }
-}
+
+
+
+        System.out.println("Задача № 3!");
+        //Распечатать все элементы всех трех массивов в обратном порядке.
+                for (int iNew = 0 ; iNew < arrSize ;iNew++){
+                   System.out.print(arrays[arrays.length-arrays[iNew]] + ",");
+
+
+                }
+        }}
+    }}}
