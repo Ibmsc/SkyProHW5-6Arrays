47f3f4353d7fd7b3574959c91aa02ee83437a07f
ibmsc <Ibm.sc1409@gmail.com>
2022-07-07 08:55:04 +0300

    HW done

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,71 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задача № 1!");
 
         int[] arrays = new int[3];
-        int arrSize = arrays.length;
         arrays[0] = 1;
         arrays[1] = 2;
         arrays[2] = 3;
 
-        double[] weightsD = {1.57, 7.654, 9.986};
+        double[] arraysD = {1.57, 7.654, 9.986};
 
-        short[] weightsS = new short[] {4,5,6,7,8};
-        int arrSizeS = arrays.length;
-        System.out.println("Задача № 2!");
+        boolean[] arraysB = {false,true,false};
 
-        for (int i = 0; i < 1; i++) {
-            System.out.println(" " + arrays[0] + ", " + arrays[1] + ", " + arrays[2] + " " );
+        System.out.println("Задача№ 2!");
 
-        for (double d = 0; d < 1; d++) {
-            System.out.println(" " + weightsD[0] + ", " + weightsD[1] + ", " + weightsD[2] + " ");
-
-            for (short s = 0; s < 1; s++) {
-                System.out.println(" " + weightsS[0] + ", " + " " + weightsS[1] + ", " + " " + weightsS[2] + ", " + " "
-                        + weightsS[3] + ", " + " " + weightsS[4] + " ");
-
-
-
+        for (int i = 0; i < arrays.length; i++) {
+            if (i < arrays.length - 1)
+            System.out.print(arrays[i] +", ");
+            else
+            System.out.println(arrays[i]);
+        }
+        for (int i = 0; i < arraysD.length; i++) {
+            if (i < arraysD.length - 1)
+                System.out.print(arraysD[i] +", ");
+            else
+                System.out.println(arraysD[i]);
+        }
+        for (int i = 0; i < arraysB.length; i++) {
+            if (i < arraysB.length - 1)
+                System.out.print(arraysB[i] +", ");
+            else
+                System.out.println(arraysB[i]);
+        }
+        System.out.println();
         System.out.println("Задача № 3!");
         //Распечатать все элементы всех трех массивов в обратном порядке.
-                for (int iNew = 0 ; iNew < arrSize ;iNew++){
-                   System.out.print(arrays[arrays.length-arrays[iNew]] + ",");
+        for (int i = arrays.length-1; i >=0 ; i--) {
+            if (i > 0)
+            System.out.print(arrays[i] + ", ");
+            else if (i == 0)
+                System.out.println(arrays[i]);
+        }
+        for (int i = arraysD.length-1; i >=0 ; i--) {
+            if (i > 0)
+                System.out.print(arraysD[i] + ", ");
+            else if (i == 0)
+                System.out.println(arraysD[i]);
+        }
+        for (int i = arraysB.length-1; i >=0 ; i--) {
+            if (i > 0)
+                System.out.print(arraysB[i] + ", ");
+            else if (i == 0)
+                System.out.println(arraysB[i]);
+        }
+        System.out.println();
+        System.out.println("Задача № 4!");
+        /*Пройдитесь по первому целочисленному массиву и все нечетные числа в нем сделайте четными (нужно прибавить 1). */
+        for (int i = 0; i< arrays.length;i++){
+            if (arrays[i]%2==1)
+                arrays[i]++;
+
+                if (i < arrays.length - 1)
+                    System.out.print(arrays[i] +", ");
+                else
+                    System.out.println(arrays[i]);
+        }
 
 
-                }
-        }}
-    }}}
+    }}
